d0f3460e243454cf18cecc861e0f10a4a65391cd
charlenebatiste <batistecharlene@gmail.com>
2021-07-15 15:53:40 -0400

    refactored app set up

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,9 +2,17 @@ import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
+//   {
+//     path: '',
+//     loadChildren: () => import('./tabs/tabs.module').then(m => m.TabsPageModule)
+//   },
   {
     path: '',
-    loadChildren: () => import('./tabs/tabs.module').then(m => m.TabsPageModule)
+    loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)
+  },
+  {
+    path: 'login',
+    loadChildren: () => import('./login/login.module').then( m => m.LoginPageModule)
   }
 ];
 @NgModule({
